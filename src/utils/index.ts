d68d869ec1e1bfa40e66a8e618ebe27de2f62192
Icheka <rhemafortune@gmail.com>
2021-02-20 22:53:36 +0100

    Fixed authRoute in utils/index.ts/_auth()

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export function _auth(): void {
-    const authRoute = "/auth";
+    const authRoute = "/#/auth";
     const user = localStorage.getItem('user');
 
     if (user === undefined || user === null) location.href = authRoute;
