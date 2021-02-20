2ac8f9cc1ca0abcf53198a5ad95519a3a3368331
Icheka <rhemafortune@gmail.com>
2021-02-20 23:04:10 +0100

    ...

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -76,7 +76,7 @@ export default class Login extends Vue {
     mounted() {
         let user_id = localStorage.getItem("user_id");
 
-        if (user_id !== null) {
+        if (user_id != null) {
             this.$router.push("/student");
         }
     }
